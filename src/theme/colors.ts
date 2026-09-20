8d4256cf0b6d6052a44327b3f2727b68ae095237
Nouman Ahmed <info.zeenox@gmail.com>
2026-09-21 04:35:55 +0500

    Add initial design system colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  background: "#0B0D10",
+  surface: "#11141A",
+  surfaceRaised: "#151922",
+  border: "#242A35",
+  text: "#F5F7FA",
+  textSecondary: "#9EA7B5",
+  textMuted: "#747E8E",
+  accent: "#DDE2EA",
+  track: "#292F3A",
+};
