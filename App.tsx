bfff44dacc34329a95779e5000ed17e68bbf5ef4
Nouman Ahmed <info.zeenox@gmail.com>
2026-09-21 08:50:51 +0500

    Integrate authentication flow before app shell

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import { AuthScreen } from "./src/screens/AuthScreen";
 import { AppShell } from "./src/navigation/AppShell";
 import { ThemeProvider } from "./src/theme/ThemeContext";
 
 export default function App() {
-  return <ThemeProvider><AppShell /></ThemeProvider>;
+  const [authenticated, setAuthenticated] = useState(false);
+  return (
+    <ThemeProvider>
+      {authenticated ? <AppShell /> : <AuthScreen onAuthenticated={() => setAuthenticated(true)} />}
+    </ThemeProvider>
+  );
 }
