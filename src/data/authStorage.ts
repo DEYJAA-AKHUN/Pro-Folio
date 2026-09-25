6dffd8e6f037bfe2ff8a2035dc15f41dcffdd3f9
Nouman Ahmed <info.zeenox@gmail.com>
2026-09-26 04:19:09 +0500

    fix: use shared Firebase initialization

--- src/data/authStorage.ts
+++ src/data/authStorage.ts
@@ -1,6 +1,7 @@
 import { Platform } from "react-native";
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth, initializeAuth, browserLocalPersistence, getReactNativePersistence, sendPasswordResetEmail, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut as firebaseSignOut, type User } from "firebase/auth";
+import { getReactNativePersistence } from "firebase/auth";
+import { getFirebaseApp } from "./firebase";
+import { getAuth, initializeAuth, browserLocalPersistence, sendPasswordResetEmail, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut as firebaseSignOut, type User } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = { apiKey:"AIzaSyArC1KYYHNbFmH87pBBCgQwsQ1bzjaAd0I", authDomain:"pro-folio-6a9d0.firebaseapp.com", projectId:"pro-folio-6a9d0", storageBucket:"pro-folio-6a9d0.firebasestorage.app", messagingSenderId:"243215346443", appId:"1:243215346443:web:b23f0576760eb9c14e900e" };
